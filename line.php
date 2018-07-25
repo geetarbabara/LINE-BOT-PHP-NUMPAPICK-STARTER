55b867f35868d3933d4840e1997710d19608a0ba
geetarbabara <41662458+geetarbabara@users.noreply.github.com>
2018-07-25 13:33:25 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,8 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'Sz2pqA7iHWmE80uHtekGhSNIAd9RmCK1Kog+cOMRZrcl79OQQVY/btWxHuE3cMHaGfpVMxoWb9eQFKi+SzGrhsAA1lEJ0aOoRXCxg5M/9zVcmjDWZSeupX/zfpfnZPWizXiCiY431aWkV2c4e+s6ZQdB04t89/1O/w1cDnyilFU=
+'; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +15,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U3962e0195f05edcb835f7d6f7aba05c7',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
